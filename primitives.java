3e029dd9cc28261c31617d4c6f88ed3073a45c9b
nishchal <nishchalchintu15@gmail.com>
2025-07-15 21:24:55 +0530

    primitives have been changed

--- primitives.java
+++ primitives.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 public class primitives{
     public static void main(String[] args) {
         Scanner input= new Scanner(System.in);
-        String who = input.next();
-        System.out.println(who);
+        int num1=input.nextInt();
+        int num2 = input.nextInt();
+        int num3 = num1 + num2 ;
+        System.out.println("the addition of 2 nos is"+ num3);
     }
 }
