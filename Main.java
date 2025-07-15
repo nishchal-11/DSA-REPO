7365b9826ad55e13297e330174174de75da43df2
nishchal <nishchalchintu15@gmail.com>
2025-07-15 21:12:26 +0530

    primitives added

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+   public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       System.out.println(input.nextLine());
+   }
 }
---
+++ primitives.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class primitives{
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        String who = input.next();
+        System.out.println(who);
+    }
+}
