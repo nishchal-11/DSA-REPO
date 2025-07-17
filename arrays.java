7bca8667e01356f827a8d4322da44515453c8779
nishchal <nishchalchintu15@gmail.com>
2025-07-17 21:10:41 +0530

    arrays modifies for loops output printing

--- arrays.java
+++ arrays.java
@@ -4,13 +4,19 @@ import java.util.Scanner;
 
 public class arrays{
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String[] arr= new  String[5];
-        for(int i=0; i < arr.length;i++){
-            arr[i] = in.next();
+        Scanner in = new Scanner(System.in);        
+        int[][] arr = new int[3][3];
 
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = in.nextInt();
+            }
         }
-        System.out.println(Arrays.toString(arr));
+        for (int[] b: arr) {
+            System.out.println(Arrays.toString(b));
+            System.out.println();
         }
-    
+        
+        
+    }
 }
